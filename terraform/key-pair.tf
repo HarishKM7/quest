84cc7c4ad1bddb646ab649a625601b4a45f7c3df
Harish KM <harishkm@harishkm.in>
2021-08-16 14:32:32 +0530

    Add EC2 key pair

---
+++ terraform/key-pair.tf
@@ -0,0 +1,10 @@
+resource "tls_private_key" "tls_key" {
+  algorithm = "RSA"
+}
+
+module "key_pair" {
+  source     = "terraform-aws-modules/key-pair/aws"
+  version    = ">= 1.0.0"
+  key_name   = "rearc-quest"
+  public_key = tls_private_key.tls_key.public_key_openssh
+}

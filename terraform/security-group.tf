13ee59fcb221ab5d8f8ecb7a409574f94e73b769
Harish KM <harishkm@harishkm.in>
2021-08-16 21:02:17 +0530

    Open port 3030 for Docker container

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -7,11 +7,18 @@ module "security_group" {
   ingress_cidr_blocks = ["0.0.0.0/0"]
   ingress_rules       = ["ssh-tcp", "http-80-tcp", "https-443-tcp"]
   egress_rules        = ["all-all"]
+
   ingress_with_cidr_blocks = [{
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
     description = "Rearc Express App"
+    cidr_blocks = "0.0.0.0/0"
+    }, {
+    from_port   = 3030
+    to_port     = 3030
+    protocol    = "tcp"
+    description = "Rearc Docker Container"
     cidr_blocks = "0.0.0.0/0"
   }]
 }

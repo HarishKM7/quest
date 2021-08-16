5bc1870cc7c7e15f1368702121dd2a17c589c3f1
Harish KM <harishkm@harishkm.in>
2021-08-16 16:41:22 +0530

    Fix EC2 instance IP for SSH

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -18,7 +18,7 @@ resource "null_resource" "ec2" {
     type        = "ssh"
     user        = "ec2-user"
     private_key = tls_private_key.tls_key.private_key_pem
-    host        = module.ec2.public_dns
+    host        = module.ec2.public_dns[0]
   }
   provisioner "file" {
     source      = "../app"

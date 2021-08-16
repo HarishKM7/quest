8e0d84297cf190b9f324606dc2f5e1274eaa7dda
Harish KM <harishkm@harishkm.in>
2021-08-16 15:42:48 +0530

    Create EC2 server for app

---
+++ terraform/ec2.tf
@@ -0,0 +1,13 @@
+module "ec2" {
+  source                      = "terraform-aws-modules/ec2-instance/aws"
+  version                     = ">= 2.19.0"
+  name                        = "rearc-quest"
+  instance_count              = 1
+  ami                         = var.amazon_linux_64_bit_ami
+  instance_type               = var.ec2_instance_type
+  key_name                    = module.key_pair.key_pair_key_name
+  subnet_id                   = var.public_subnet_1
+  vpc_security_group_ids      = [module.security_group.security_group_id]
+  associate_public_ip_address = true
+  # user_data                   = file("ec2-user-data.sh")
+}

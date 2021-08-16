038bb5cea54c39155f873f2ec19033d4914f862e
Harish KM <harishkm@harishkm.in>
2021-08-16 15:41:11 +0530

    Add variables for VPC & EC2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,17 @@
 variable "aws_region" {
   description = "AWS region code where this infra is deployed."
 }
+
+variable "vpc_id" {
+  description = "ID of the VPC where this infra is deployed."
+}
+variable "public_subnet_1" {
+  description = "A public subnet for the EC2 instance."
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type."
+}
+variable "amazon_linux_64_bit_ami" {
+  description = "AMI ID of Amazon Linux 64-bit in this region."
+}

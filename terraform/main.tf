450a5aaefa1e4e12b738d7e7bbcadc14bdec75f9
Harish KM <harishkm@harishkm.in>
2021-08-16 15:00:52 +0530

    Set remote backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,14 @@
 terraform {
   required_version = ">= 1.0.4"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.54.0"
     }
+  }
+  backend "remote" {
+    organization = "HarishKM"
+    workspaces { name = "quest" }
   }
 }
 

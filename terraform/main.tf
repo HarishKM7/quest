afbb317d07bcf8c1e8aeb197b9206edc16f00f4c
Harish KM <harishkm@harishkm.in>
2021-08-18 14:26:54 +0530

    Add comment about backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,9 @@ terraform {
       version = ">= 3.54.0"
     }
   }
+
+  # To create this infra in your own AWS account,
+  # remove this backend block.
   backend "remote" {
     organization = "HarishKM"
     workspaces { name = "quest" }
